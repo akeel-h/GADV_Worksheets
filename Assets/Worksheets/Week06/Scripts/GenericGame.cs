3535ff598159a5868718279a21dffd35d3dc2b6b
akeel-h <akilabdul1912@gmail.com>
2025-07-16 14:35:57 +0800

    Week 06 Introduction to Generics
    Finished worksheet on the introduction to Generics

---
+++ Assets/Worksheets/Week06/Scripts/GenericGame.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GenericsGame : MonoBehaviour
+{
+    public GameObject AsteroidPrefab;
+
+    void Start()
+    {
+        SpawnAsteroid(AsteroidPrefab, new Vector3(0, 0, 0));
+    }
+
+    public GameObject SpawnAsteroid(GameObject asteroidPrefab, Vector3 position)
+
+    {
+        // The non-generic approach, which returns a GameObject
+        GameObject clone = Instantiate(asteroidPrefab, position, Quaternion.identity);
+
+        // The generic approach, which returns the actual type 
+        // specified by <T>. Note that the prefab must have a 
+        // component of the specified type, e.g. ateroidPrefab must // have an Asteroid component (script) attached to it.
+        clone = Instantiate<GameObject>(asteroidPrefab);
+
+        Debug.Log("Spawned: " + clone.name);
+        return clone;
+    }
+}
+
+
